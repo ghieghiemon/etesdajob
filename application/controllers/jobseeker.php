f8bde0163d83834744d4f0c1fe3da0d54371c2fe
shenne <shen.layug@gmail.com>
2013-10-03 22:09:21 +0800

    jobseeker controller

---
+++ application/controllers/jobseeker.php
@@ -0,0 +1,119 @@
+<?php
+
+class Jobseeker extends CI_Controller {
+    //jobseeker
+    public function jobseeker_header()
+    {
+        $this->load->model('model_jobseeker');
+        $data['name'] = $this->model_jobseeker->get_jsname();
+        $data['pic'] = $this->model_jobseeker->get_jspic();
+        $this->load->view('jobseeker/header', $data);
+    }
+    public function jobseeker_dashboard()
+    {
+        $this->jobseeker_header();
+       // $this->load->view('jobseeker/JSDash');
+        $this->load->view('footer');
+    }
+    
+    public function jobseeker_myappspage()
+    {
+        $this->load->model('model_main');
+        $this->load->model('model_jobseeker');
+        $id = $this->model_main->get_appid($this->session->userdata('email'));
+        $data['invites'] = $this->model_jobseeker->get_myinvites($id);
+        $data['myapp'] = $this->model_jobseeker->get_myapplications($id);
+        
+        
+        $this->jobseeker_header();
+        $this->load->view('jobseeker/JSMyApps', $data);
+        $this->load->view('footer');
+    }
+    public function jobseeker_myappsdetail($jobno)
+    {
+        $this->load->model('model_main');
+        $this->load->model('model_jobseeker');
+        $id = $this->model_main->get_appid($this->session->userdata('email'));
+        $data['appdetails'] = $this->model_jobseeker->get_jobdetails($jobno);
+        $data['myapps'] =$this->model_jobseeker->get_mysideapplications($id,$jobno);
+        
+        $this->jobseeker_header();
+        $this->load->view('jobseeker/JSMyAppDetail',$data);
+        $this->load->view('footer');
+    }
+    public function match($jobno, $id)
+    {
+        $this->load->model('model_main');
+        $this->load->model('model_jobseeker');
+        $matchedCert = $this->model_jobseeker->get_matchedCert($jobno, $id);
+        $requiredCert = $this->model_jobseeker->get_jobcert($jobno);
+        
+        if(!($matchedCert < $requiredCert))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+            
+    }
+    public function jobseeker_jobmarketpage()
+    {
+        $this->load->model('model_main');
+        $this->load->model('model_jobseeker');
+        $id = $this->model_main->get_appid($this->session->userdata('email'));
+        $jobs = $this->model_jobseeker->get_alljobs();
+        $dob = $this->model_jobseeker->get_dob($id);
+        $sex = $this->model_jobseeker->get_sex($id);
+        
+        $qualified = $this->model_jobseeker->get_qualifiedjobs($dob, $sex);
+        
+        $suggested = array();
+        foreach($jobs as $a)
+        {
+            
+            if($this->match($a['jobno'],$id))
+            {
+                array_push($suggested, $a);
+            }
+        }
+        
+        foreach($qualified as $a)
+        {
+            $jobno[]=$a['jobno'];
+        }
+        
+        $final = array();
+        foreach ($suggested as $a)
+        {
+            if(in_array($a['jobno'],$jobno))
+                    array_push($final, $a);
+        }
+        $data['suggested'] = $final;
+        $this->jobseeker_header();
+        $this->load->view('jobseeker/JSJobMarket',$data);
+        $this->load->view('footer');
+    }  
+    
+    public function apply_job($jobno,$invno)
+    {
+        $this->load->model('model_main');
+        $this->load->model('model_jobseeker');
+        $id = $this->model_main->get_appid($this->session->userdata('email'));
+        $this->model_jobseeker->apply_job($id,$jobno);
+        $this->model_jobseeker->accept_job($invno);
+        
+        $this->jobseeker_myappsdetail($jobno);
+    }
+    public function decline_job($invno)
+    {
+        $this->load->model('model_main');
+        $this->load->model('model_jobseeker');
+        $this->model_jobseeker->decline_job($invno);
+        
+        
+        $this->jobseeker_myappspage();
+    }
+}
+?>
