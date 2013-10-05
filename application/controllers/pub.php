0271477e3d7f14bca2f01f76a1e1f2e4ec65a31d
gowueld <emmanuel_ydel@dlsu.ph>
2013-10-05 20:26:04 +0800

    Updated pub.php controller

--- application/controllers/pub.php
+++ application/controllers/pub.php
@@ -12,15 +12,29 @@ class Pub extends CI_Controller {
        $this->load->view("footer");
    }
    
-   public function search_industries($sectorid)
+   public function search_industries($sectorid = "")
    {
-       $this->load->model('model_pub');
-        
-       $data['industries'] = $this->model_pub->get_industryVacancies();
-       
-       $this->load->view("public/header");
-       $this->load->view("public/PIndustryResult",$data);
-       $this->load->view("footer");
+		
+		if($sectorid == ""):
+		
+			echo "sectorid doesn't exist";
+			return;
+		
+		endif;
+   
+		$this->load->model('model_pub');
+		$data['industries'] = $this->model_pub->get_industryVacancies();
+		$this->load->view("public/header");
+		$this->load->view("public/PIndustryResult",$data);
+		$this->load->view("footer");
    }
+   
+   public function print_industries(){
+   
+		$this->load->model('model_pub');
+		print_r($this->model_pub->get_industryVacancies());
+		
+   }
+   
 }
 ?>
