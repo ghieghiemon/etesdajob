4a9f3fe188c529c2f1eb714292db4a2e1459cd76
shenne <shen.layug@gmail.com>
2013-10-07 20:28:22 +0800

    updated controller: js, main and pub

--- application/controllers/jobseeker.php
+++ application/controllers/jobseeker.php
@@ -2,11 +2,11 @@
 
 class Jobseeker extends CI_Controller {
     //jobseeker
-    public function jobseeker_header()
+      public function jobseeker_header()
     {
-        $this->load->model('model_jobseeker');
-        $data['name'] = $this->model_jobseeker->get_jsname();
-        $data['pic'] = $this->model_jobseeker->get_jspic();
+        $this->load->model('model_main');
+        $data['name'] = $this->model_main->get_jsname();
+        $data['pic'] = $this->model_main->get_jspic();
         $this->load->view('jobseeker/header', $data);
     }
     public function jobseeker_dashboard()
@@ -19,11 +19,9 @@ class Jobseeker extends CI_Controller {
     public function jobseeker_myappspage()
     {
         $this->load->model('model_main');
-        $this->load->model('model_jobseeker');
         $id = $this->model_main->get_appid($this->session->userdata('email'));
-        $data['invites'] = $this->model_jobseeker->get_myinvites($id);
-        $data['myapp'] = $this->model_jobseeker->get_myapplications($id);
-        
+        $data['invites'] = $this->model_main->get_myinvites($id);
+        $data['myapp'] = $this->model_main->get_myapplications($id);
         
         $this->jobseeker_header();
         $this->load->view('jobseeker/JSMyApps', $data);
@@ -31,11 +29,9 @@ class Jobseeker extends CI_Controller {
     }
     public function jobseeker_myappsdetail($jobno)
     {
-        $this->load->model('model_main');
-        $this->load->model('model_jobseeker');
         $id = $this->model_main->get_appid($this->session->userdata('email'));
-        $data['appdetails'] = $this->model_jobseeker->get_jobdetails($jobno);
-        $data['myapps'] =$this->model_jobseeker->get_mysideapplications($id,$jobno);
+        $data['appdetails'] = $this->model_main->get_jobdetails($jobno);
+        $data['myapps'] =$this->model_main->get_mysideapplications($id,$jobno);
         
         $this->jobseeker_header();
         $this->load->view('jobseeker/JSMyAppDetail',$data);
@@ -44,9 +40,8 @@ class Jobseeker extends CI_Controller {
     public function match($jobno, $id)
     {
         $this->load->model('model_main');
-        $this->load->model('model_jobseeker');
-        $matchedCert = $this->model_jobseeker->get_matchedCert($jobno, $id);
-        $requiredCert = $this->model_jobseeker->get_jobcert($jobno);
+        $matchedCert = $this->model_main->get_matchedCert($jobno, $id);
+        $requiredCert = $this->model_main->get_jobcert($jobno);
         
         if(!($matchedCert < $requiredCert))
         {
@@ -61,24 +56,20 @@ class Jobseeker extends CI_Controller {
     public function jobseeker_jobmarketpage()
     {
         $this->load->model('model_main');
-        $this->load->model('model_jobseeker');
         $id = $this->model_main->get_appid($this->session->userdata('email'));
-        $jobs = $this->model_jobseeker->get_alljobs();
-        $dob = $this->model_jobseeker->get_dob($id);
-        $sex = $this->model_jobseeker->get_sex($id);
-        
-        $qualified = $this->model_jobseeker->get_qualifiedjobs($dob, $sex);
+        $jobs = $this->model_main->get_alljobs();
+        $dob = $this->model_main->get_dob($id);
+        $sex = $this->model_main->get_sex($id);
+        $qualified = $this->model_main->get_qualifiedjobs($dob, $sex);
         
         $suggested = array();
         foreach($jobs as $a)
         {
-            
             if($this->match($a['jobno'],$id))
             {
                 array_push($suggested, $a);
             }
         }
-        
         foreach($qualified as $a)
         {
             $jobno[]=$a['jobno'];
@@ -90,28 +81,36 @@ class Jobseeker extends CI_Controller {
             if(in_array($a['jobno'],$jobno))
                     array_push($final, $a);
         }
+        
+        
         $data['suggested'] = $final;
+        
+        $data['jobs'] = $jobs;
         $this->jobseeker_header();
         $this->load->view('jobseeker/JSJobMarket',$data);
         $this->load->view('footer');
     }  
-    
-    public function apply_job($jobno,$invno)
+    public function apply_jobinvite($jobno,$invno)
     {
         $this->load->model('model_main');
-        $this->load->model('model_jobseeker');
         $id = $this->model_main->get_appid($this->session->userdata('email'));
-        $this->model_jobseeker->apply_job($id,$jobno);
-        $this->model_jobseeker->accept_job($invno);
+        $this->model_main->apply_job($id,$jobno);
+        $this->model_main->accept_job($invno);
+        
+        $this->jobseeker_myappsdetail($jobno);
+    }
+    public function apply_job($jobno)
+    {
+        $this->load->model('model_main');
+        $id = $this->model_main->get_appid($this->session->userdata('email'));
+        $this->model_main->apply_job($id,$jobno);
         
         $this->jobseeker_myappsdetail($jobno);
     }
     public function decline_job($invno)
     {
         $this->load->model('model_main');
-        $this->load->model('model_jobseeker');
-        $this->model_jobseeker->decline_job($invno);
-        
+        $this->model_main->decline_job($invno);
         
         $this->jobseeker_myappspage();
     }
--- application/controllers/pub.php
+++ application/controllers/pub.php
@@ -24,6 +24,65 @@ class Pub extends CI_Controller {
         $this->load->view("public/PIndustryResult",$data);
         $this->load->view("footer");
    }
+   
+   public function pub_jobmarketpage()
+    {
+        $this->load->model('model_main');
+        $data['drpindustries'] = $this->model_main->get_drpindustries();
+        $data['regions'] = $this->model_main->get_regions();
+        $this->load->view('public/header');
+        $this->load->view('public/PJobMarket');
+    }
+    
+        public function pub_searchjob()
+    {
+        $this->load->model('model_main');
+        $data['drpindustries'] = $this->model_main->get_drpindustries();
+        $data['regions'] = $this->model_main->get_regions();
+        $data['search'] = $this->search_job();
+   
+        
+        $this->load->view('public/header');
+        $this->load->view('public/PJobMarket',$data);
+    }
+    
+        public function pub_alljob()
+    {
+        $this->load->model('model_main');
+        $data['drpindustries'] = $this->model_main->get_drpindustries();
+        $data['regions'] = $this->model_main->get_regions();
+        $data['search'] = $this->all_job();
+   
+        
+        $this->load->view('public/header');
+        $this->load->view('public/PJobMarket',$data);
+    }
+    
+    public function search_job(){
+            //printf($this->input->post('jobname'));
+            $this->load->model('model_main');
+            $this->load->model('model_pub');
+            $search = $this->model_pub->search_job($this->input->post('JT'),
+                     ($this->input->post('industry')),($this->input->post('cityid')),
+                     ($this->input->post('COMP')));
+            return $search;        
+   }
+   
+       public function all_job(){
+            //printf($this->input->post('jobname'));
+            $this->load->model('model_main');
+            $this->load->model('model_pub');
+            $search = $this->model_pub->all_job();
+            return $search;        
+   }
+   
+    public function pevent_details($eno){
+    $this->load->model('model_main');
+    $data['details'] = $this->model_main->get_eventdetails($eno);    
+    $this->load->view('public/header');
+    $this->load->view('public/PEventDetails', $data);
+    //$this->load->view('footer');
+}
     
 }
 ?>
