5104749ca18ac2774e8777afbf24a26c62886719
ghieghiemon <ghieguerrero@gmail.com>
2013-10-09 20:45:00 +0800

    league main

--- application/models/model_pub.php
+++ application/models/model_pub.php
@@ -145,6 +145,15 @@ class Model_pub extends CI_Model {
         return $query->result_array();
         $db1->close();
     }
+    public function get_leagues()
+    {
+        $db1 = $this->load->database('local', TRUE);
+        $query = $db1->query("SELECT l.*, COUNT(m.userid) as members FROM league l 
+                                JOIN league_members m ON l.leagueno = m.leagueno 
+                                GROUP BY m.leagueno ORDER BY members");
+        return $query->result_array();
+        $db1->close();
+    }
 }
 
 
