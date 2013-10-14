22f83211ddb33eb0f982534996a0e5af880d0aed
gowueld <emmanuel_ydel@dlsu.ph>
2013-10-14 23:33:49 +0800

    Revert "new bootstrap"
    This reverts commit 2f85cf7ba28b025348e70c94dd253d4a464132d9.

--- application/views/footer.php
+++ application/views/footer.php
@@ -43,6 +43,7 @@
         <!-- Le javascript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
+     <script src="<?php echo base_url()?>assets/bootstrap/js/jquery.js"></script>
     <script src="<?php echo base_url()?>assets/bootstrap/js/bootstrap-transition.js"></script>
     <script src="<?php echo base_url()?>assets/bootstrap/js/bootstrap-alert.js"></script>
     <script src="<?php echo base_url()?>assets/bootstrap/js/bootstrap-modal.js"></script>
@@ -55,20 +56,20 @@
     <script src="<?php echo base_url()?>assets/bootstrap/js/bootstrap-collapse.js"></script>
     <script src="<?php echo base_url()?>assets/bootstrap/js/bootstrap-carousel.js"></script>
     <script src="<?php echo base_url()?>assets/bootstrap/js/bootstrap-typeahead.js"></script>
-<!--    <script src="http://code.jquery.com/jquery-latest.js"></script>
-    <script src="j<?php echo base_url()?>assets/bootstrap/s/bootstrap.min.js"></script>-->
+    <script src="http://code.jquery.com/jquery-latest.js"></script>
+    <script src="j<?php echo base_url()?>assets/bootstrap/s/bootstrap.min.js"></script>
     
-<!--    <script type="text/javascript" src="<?php echo base_url()?>assets/bootstrap/js/jquery.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url()?>assets/bootstrap/js/jquery.min.js"></script>
 
 
 <script type="text/javascript" src="<?php echo base_url()?>assets/bootstrap/js/jquery.fancybox-1.3.4.pack.js"></script>
 
- Add jQuery library 
+<!-- Add jQuery library -->
 <script type="text/javascript" src="<?php echo base_url()?>assets/bootstrap/js/jquery-latest.min.js"></script>
 
- Add mousewheel plugin (this is optional) 
+<!-- Add mousewheel plugin (this is optional) -->
 <script type="text/javascript" src="<?php echo base_url()?>assets/bootstrap/js/jquery.mousewheel-3.0.6.pack.js"></script>
--->
+
 
 <!-- Add fancyBox -->
 <link rel="stylesheet" href="<?php echo base_url()?>assets/bootstrap/js/jquery.fancybox.css?v=2.1.4" type="text/css" media="screen" />
