f6331910b50e7bf81e3c8fdb8077ab4ab91918dc
ghieghiemon <ghieguerrero@gmail.com>
2013-11-09 20:38:18 +0800

    employer model

--- application/models/model_employer.php
+++ application/models/model_employer.php
@@ -221,7 +221,7 @@ class Model_employer extends CI_Model {
                            JOIN etesda.reference_city c ON c.cityid = v.city
                            JOIN etesda.reference_region r ON r.regionid = v.region
                            WHERE companyID = $id AND vacanciesleft >0
-                           ORDER BY jobtitle ASC
+                           ORDER BY lastedited DESC
                            
                            ");
         return $sql->result_array();
