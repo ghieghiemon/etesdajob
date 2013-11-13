320573c440d1baeb12d08d73677d5b8f45346d94
shenne <shen.layug@gmail.com>
2013-11-13 18:05:35 +0800

    Event Pages

---
+++ application/views/employer/EEventInvite.php
@@ -0,0 +1,126 @@
+<div class="container">
+<div style="margin-left: 1%; margin-top: 1%;  margin-bottom:-7%">
+	
+    <div class="row-fluid">
+    	<div class="span12">
+        	<div class="well">
+            	<h3 class="media-heading">
+                    <img src="assets/img/icons/glyphicons_195_circle_info.png" width="25"> Invite Job Seekers
+                </h3>
+                
+                <div style="width:1200px;height:470px;overflow:auto;"><!--start scrollable table-->      
+                      <table class="tableInv table-hover table-condensed table-striped">
+                      <thead>
+                          <tr>
+                          	  <th class="span1" style="text-align:center">
+                              	<button class="btn btn-info btn-mini" style="margin-top:5px;">Invite</button><br>
+                              	<input type="checkbox">
+                              </th>
+                              <th class="span3" style="text-align:center">Name</th>
+                              <th class="span2" style="text-align:center">Age</th>
+                              <th class="span2" style="text-align:center">Gender</th>
+                              <th class="span2" style="text-align:center">Location</th>
+                              <th class="span2" style="text-align:center">Date Graduated</th>
+                              <th class="span3" style="text-align:center">Years of Work</th>
+                          </tr>
+                      </thead>
+                      
+                      <tbody class="recName">
+                          <tr>
+                          	  <td>
+                              	<input type="checkbox">
+                              </td>
+                              
+                              <td>
+                              	 <a href="#" class="recAppName">
+                                     Angelica Guerrero
+                                 </a>
+                              </td>
+                              
+                              <td>
+                                  29
+                              </td>
+                              
+                              <td>
+                              	  M
+                              </td>
+                              
+                              <td>
+                                  NCR | Makati City
+                              </td>
+                              
+                              <td>
+                              	Feb 2004
+                              </td>
+                              
+                              <td>
+                                  8
+                              </td>
+                          </tr>
+                          
+                          <tr>
+                              <td>
+                              	<input type="checkbox">
+                              </td>
+                              
+                              <td>
+                                 <a href="#" class="recAppName">
+                                   Ruben Mendoza
+                                 </a>
+                              </td>
+                              
+                              <td>
+                                  29
+                              </td>
+                             
+                              <td>
+                              	  M
+                              </td>
+                              
+                              <td>
+                                  NCR | Makati City
+                              </td>
+                              
+                              <td>
+                                March 2007
+                              </td>
+                              
+                              <td>
+                                  2
+                              </td>
+                          </tr>                         
+                          
+                      </tbody>
+                  </table>	
+                  </div><!--end scrollable-->   
+            </div><!--end well-->
+        </div><!--end span left folumn-->
+        
+        
+    </div><!--end row-->
+
+</div><!--End div-->
+</div><!--End Container fluid-->
+ 
+            
+             
+
+
+      <hr>
+
+<script type="text/javascript">
+       
+   $(document).ready(function(){
+	  
+	   $('#test').dataTable({
+			"sPaginationType": "full_numbers"
+		});
+	   
+   });
+	
+</script>
+
+    
+</body>
+
+</html>
