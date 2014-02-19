ace74f64bf9ea8a1d16ae214729ee6c60d1e19fd
shenne <shen.layug@gmail.com>
2014-02-19 22:41:07 +0800

    public job market

--- application/controllers/pub.php
+++ application/controllers/pub.php
@@ -15,7 +15,8 @@ class Pub extends CI_Controller {
    public function search_industries($sectorid)
    {	
         $this->load->model('model_pub');
-        
+        $this->load->model('model_jobseeker');
+        $data['jobs'] =  $this->model_jobseeker->get_alljobs();
         $data['industries'] = $this->model_pub->get_industryVacancies();
         $data['vacancies'] = $this->model_pub->get_perIndustryVacancies($sectorid);
         $data['sectorName'] = $this->model_pub->get_industryName($sectorid);
