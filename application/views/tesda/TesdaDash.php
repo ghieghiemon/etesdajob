fbc750fe92ae2a5acacb661c689f40cc58a6ade4
ghieghiemon <ghieguerrero@gmail.com>
2014-03-06 07:45:48 +0800

    rainfall

--- application/views/tesda/TesdaDash.php
+++ application/views/tesda/TesdaDash.php
@@ -42,19 +42,8 @@ $(function () {
                     }
                 }
             }, { // Secondary yAxis
-                title: {
-                    text: 'Rainfall',
-                    style: {
-                        color: '#4572A7'
-                    }
-                },
-                labels: {
-                    format: '{value}',
-                    style: {
-                        color: '#4572A7'
-                    }
-                },
-                opposite: true
+                
+               
             }],
             tooltip: {
                 shared: true
