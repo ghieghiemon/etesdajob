0c75a2662faf8b76d697460031c00e526e3e88ac
shenne <shen.layug@gmail.com>
2014-03-06 11:27:53 +0800

    header fix

--- application/views/employer/header.php
+++ application/views/employer/header.php
@@ -303,7 +303,8 @@ function checkall3(el){
     <script src="<?php echo base_url()?>assets/bootstrap/js/bootstrap-collapse.js"></script>
     <script src="<?php echo base_url()?>assets/bootstrap/js/bootstrap-carousel.js"></script>
     <script src="<?php echo base_url()?>assets/bootstrap/js/bootstrap-typeahead.js"></script>
-    <script src="<?php echo base_url()?>assets/bootstrap/js/jquery-latest.js"></script>
+    <!--<script src="<?php echo base_url()?>assets/bootstrap/js/jquery-latest.js"></script>-->
+    <script src="http://code.jquery.com/jquery-latest.js"></script>
     <script src="<?php echo base_url()?>assets/bootstrap/js/bootstrap.min.js"></script>
     <script src="<?php echo base_url()?>assets/bootstrap/js/datatable.js"></script>
 <!--postV modal end-->
