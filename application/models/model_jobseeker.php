78ff0ee3cd9ea03a0cc07f4b941a1d4899890b78
shenne <shen.layug@gmail.com>
2014-03-09 15:01:21 +0800

    briefcase

--- application/models/model_jobseeker.php
+++ application/models/model_jobseeker.php
@@ -566,7 +566,7 @@ class Model_jobseeker extends CI_Model {
         join etesda.schedule_slots ss on ss.scheduleid = sc.scheduleid
         JOIN tesda_centraldb.employer_profile p ON p.userID = j.companyid
         where (a.status = 'Interview' or a.status ='Exam') and ss.appid =$id
-                 AND scheduledate >= curdate()group by ss.slotid ");
+                 AND scheduledate >= curdate()group by ss.slotid ORDER BY scheduledate ASC ");
         return $query->result_array();
 
 
