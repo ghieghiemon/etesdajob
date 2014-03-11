dd6a4e114033a76185526a60a6ff0e2386c3f332
shenne <shen.layug@gmail.com>
2014-03-11 13:36:06 +0800

    last na ulit

--- application/views/employer/EVacancies.php
+++ application/views/employer/EVacancies.php
@@ -145,14 +145,14 @@ foreach ($myvacancies as $a)
                                                   $diff = abs(strtotime($date2) - strtotime($date));
 
                                                   $days = round((($diff/24)/60)/60);
-                                                  if ($a['expirationdate'] > $a['currentdate'])
+                                                  if ($a['exp'] > $a['currentdate'])
                                                   {
                                                   echo $days;
                                                   echo " days left";
                                                   }
                                                   else
                                                   {
-                                                    echo "EXPIRED";
+                                                    echo "<font color ='red'>EXPIRED </font>";
                                                   }
                                                 ?>
                                       
