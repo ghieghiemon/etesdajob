8bcce74981f8fb6c7b047cfa704ebe68892f07e1
Nero Patrick Roque <neropatrickroque@gmail.com>
2014-03-24 00:17:47 +0800

    ereport2 update

--- application/views/employer/EReport3.php
+++ application/views/employer/EReport3.php
@@ -3,10 +3,10 @@
  <script src="http://code.highcharts.com/highcharts.js"></script>
 <script src="http://code.highcharts.com/modules/exporting.js"></script>
 <script>
-$(function () {
+    $(function () {
         $('#container').highcharts({
             chart: {
-                zoomType: 'xy'
+                type: 'column'
             },
             title: {
                 text: 'Annual Graduates Vs. Hired Graduates'
@@ -17,39 +17,28 @@ $(function () {
             xAxis: [{
                 categories: ['<?php echo $year1?>', '<?php echo $year2?>']
             }],
-            yAxis: [{ // Primary yAxis
-                labels: {
-                    format: '{value}',
-                    style: {
-                        color: '#89A54E'
-                    }
-                },
+            yAxis: {
+                min: 0,
                 title: {
-                    text: 'No. of Graduates',
-                    style: {
-                        color: '#89A54E'
-                    }
+                   text: 'Percentage of Graduates'
                 }
-            }, { // Secondary yAxis
-               
-            }],
-            tooltip: {
-                shared: true
             },
-            legend: {
-                layout: 'vertical',
-                align: 'left',
-                x: 120,
-                verticalAlign: 'top',
-                y: 100,
-                floating: true,
-                backgroundColor: '#FFFFFF'
+            tooltip: {
+                headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                    '<td style="padding:0"><b>{point.y:.1f} %</b></td></tr>',
+                footerFormat: '</table>',
+                shared: true,
+                useHTML: true
+            },
+            plotOptions: {
+                column: {
+                    pointPadding: 0.2,
+                    borderWidth: 0
+                }
             },
             series: [{
                 name: 'Annual Graduates',
-                color: '#4572A7',
-                type: 'column',
-                yAxis: 1,
                 data: [<?php
                       $ctr = count($indexedReportData);
                       foreach ($indexedReportData as $a)
@@ -59,15 +48,10 @@ $(function () {
                               echo ", ";
                           $ctr--;
                       }
-                      ?>],
-                tooltip: {
-                    valueSuffix: ' mm'
-                }
+                      ?>]
     
             }, {
                 name: 'Hired Graduates',
-                color: '#89A54E',
-                type: 'spline',
                 data: [<?php
                       $ctr = count($indexedReportData2);
                       foreach ($indexedReportData2 as $b)
@@ -77,14 +61,97 @@ $(function () {
                               echo ", ";
                           $ctr--;
                       }
-                      ?>],
-                tooltip: {
-                    valueSuffix: '°C'
-                }
+                      ?>]
+    
             }]
         });
     });
     
+//
+//    
+//    
+//$(function () {
+//        $('#container').highcharts({
+//            chart: {
+//                zoomType: 'xy'
+//            },
+//            title: {
+//                text: 'Annual Graduates Vs. Hired Graduates'
+//            },
+//            subtitle: {
+//                text: 'TESDA'
+//            },
+//            xAxis: [{
+//                categories: ['<?php echo $year1?>', '<?php echo $year2?>']
+//            }],
+//            yAxis: [{ // Primary yAxis
+//                labels: {
+//                    format: '{value}',
+//                    style: {
+//                        color: '#89A54E'
+//                    }
+//                },
+//                title: {
+//                    text: 'No. of Graduates',
+//                    style: {
+//                        color: '#89A54E'
+//                    }
+//                }
+//            }, { // Secondary yAxis
+//               
+//            }],
+//            tooltip: {
+//                shared: true
+//            },
+//            legend: {
+//                layout: 'vertical',
+//                align: 'left',
+//                x: 120,
+//                verticalAlign: 'top',
+//                y: 100,
+//                floating: true,
+//                backgroundColor: '#FFFFFF'
+//            },
+//            series: [{
+//                name: 'Annual Graduates',
+//                color: '#4572A7',
+//                type: 'column',
+//                yAxis: 1,
+//                data: [<?php
+                      $ctr = count($indexedReportData);
+                      foreach ($indexedReportData as $a)
+                      {
+                          echo $a;
+                          if($ctr > 1)
+                              echo ", ";
+                          $ctr--;
+                      }
+                      ?>//],
+//                tooltip: {
+//                    valueSuffix: ' mm'
+//                }
+//    
+//            }, {
+//                name: 'Hired Graduates',
+//                color: '#89A54E',
+//                type: 'spline',
+//                data: [<?php
+                      $ctr = count($indexedReportData2);
+                      foreach ($indexedReportData2 as $b)
+                      {
+                          echo $b;
+                          if($ctr > 1)
+                              echo ", ";
+                          $ctr--;
+                      }
+                      ?>//],
+//                tooltip: {
+//                    valueSuffix: '°C'
+//                }
+//            }]
+//        });
+//    });
+//    
 
 		</script>
            
