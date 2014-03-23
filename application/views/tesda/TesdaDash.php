14ed1a2b8b1cacf1b99bb224bcbd85c9da6877aa
Nero Patrick Roque <neropatrickroque@gmail.com>
2014-03-24 01:09:44 +0800

    update tesda dash

--- application/views/tesda/TesdaDash.php
+++ application/views/tesda/TesdaDash.php
@@ -16,7 +16,7 @@
 $(function () {
         $('#container1').highcharts({
             chart: {
-                zoomType: 'xy'
+                type: 'column'
             },
             title: {
                 text: 'Annual Graduates Vs. Hired Graduates'
@@ -27,40 +27,28 @@ $(function () {
             xAxis: [{
                 categories: ['<?php echo $year1?>', '<?php echo $year2?>']
             }],
-            yAxis: [{ // Primary yAxis
-                labels: {
-                    format: '{value}',
-                    style: {
-                        color: '#89A54E'
-                    }
-                },
+            yAxis: {
+                min: 0,
                 title: {
-                    text: 'No. of Graduates',
-                    style: {
-                        color: '#89A54E'
-                    }
+                   text: 'Percentage of Graduates'
                 }
-            }, { // Secondary yAxis
-                
-               
-            }],
-            tooltip: {
-                shared: true
             },
-            legend: {
-                layout: 'vertical',
-                align: 'left',
-                x: 120,
-                verticalAlign: 'top',
-                y: 100,
-                floating: true,
-                backgroundColor: '#FFFFFF'
+            tooltip: {
+                headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                    '<td style="padding:0"><b>{point.y:.1f} %</b></td></tr>',
+                footerFormat: '</table>',
+                shared: true,
+                useHTML: true
+            },
+            plotOptions: {
+                column: {
+                    pointPadding: 0.2,
+                    borderWidth: 0
+                }
             },
             series: [{
                 name: 'Annual Graduates',
-                color: '#4572A7',
-                type: 'column',
-                yAxis: 1,
                 data: [<?php
                       $ctr = count($indexedReportData);
                       foreach ($indexedReportData as $a)
@@ -70,15 +58,10 @@ $(function () {
                               echo ", ";
                           $ctr--;
                       }
-                      ?>],
-                tooltip: {
-                    valueSuffix: ' mm'
-                }
+                      ?>]
     
             }, {
                 name: 'Hired Graduates',
-                color: '#89A54E',
-                type: 'spline',
                 data: [<?php
                       $ctr = count($indexedReportData2);
                       foreach ($indexedReportData2 as $b)
@@ -88,10 +71,8 @@ $(function () {
                               echo ", ";
                           $ctr--;
                       }
-                      ?>],
-                tooltip: {
-                    valueSuffix: '°C'
-                }
+                      ?>]
+    
             }]
         });
     });
