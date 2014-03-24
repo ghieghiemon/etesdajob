2499635395cc79e3b10f27fb225f06e8d17f9155
ghieghiemon <ghieguerrero@gmail.com>
2014-03-25 00:19:31 +0800

    report

--- application/views/tesda/TReports.php
+++ application/views/tesda/TReports.php
@@ -118,6 +118,64 @@
         </div>
         </form>  
 </div>   
+
+   <div class="modal hide fade" id="industries">
+            <div class="modal-header">
+    	<a class="close" data-dismiss="modal">x</a>
+    	<h3>Generate Report</h3>
+  	</div>
+                        
+         <form method="post" action="<?php echo base_url()?>tesda/generate_report">
+  	<div class="modal-body"> <!--Modal members-->
+  		<div style="margin-left: 5%; margin-top: 1%">
+                    
+  			<div class="nav" style="height:220px;margin-bottom:-5px;margin-left:140px;overflow:auto;">
+                        <input type="hidden" value="9" name="reporttype">
+                           <?php    
+                            $cert['0'] = 'Choose Certification';
+                            $params = 'id="lstcert1" '; 
+                           echo form_dropdown('certs', $cert,'0',$params);    
+                                                               
+                               ?> 
+                              <br>
+                            <select name="month" id="month" class="formlist">
+                           <option value="0">Choose Month</option>
+                             <option value = "1">January</option>
+                            <option value = "2">February</option>
+                            <option value = "3">March</option>
+                            <option value = "4">April</option>
+                            <option value = "5">May</option>
+                            <option value = "6">June</option>
+                            <option value = "7">July</option>
+                            <option value = "8">August</option>
+                            <option value = "9">September</option>
+                            <option value = "10">October</option>
+                            <option value = "11">November</option>
+                            <option value = "12">December</option> 
+                            </select>
+                            
+                            <br>
+                             <select name="year">
+                                 <option>Choose Year</option>
+                                 <option>2014</option>
+                                <option>2013</option>
+                                <option>2012</option>
+                                <option>2011</option>
+                                <option>2010</option>
+                                <option>2009</option>
+                            </select>
+                        
+                </div><!--end scrollable-->
+            
+    	</div><!--end modal cert-->
+ 	</div><!--end modal-footer-->
+        <div class="modal-footer">
+            <button type="submit" class="btn btn-info" >Generate</button>
+            <a href="#" class="btn btn-primary" data-dismiss="modal">Cancel</a> 
+        </div>
+        </form>  
+</div>  
+
 <div class="modal hide fade" id="useful">
             <div class="modal-header">
     	<a class="close" data-dismiss="modal">x</a>
@@ -183,7 +241,8 @@
                             <br>
                             <select name="reporttype" id="cert">
                                 <option>Choose type of report</option>
-                                <option value = "1">Executive Summary</option>
+                                <option value = "11">Executive Summary</option>
+                                <option value="1"> Summary </option>
                                 <option value = "4">Employment per Industry</option>
                                 <option value = "5">Employment per Region</option>
                                 <option value = "2">In Demand Jobs</option>
