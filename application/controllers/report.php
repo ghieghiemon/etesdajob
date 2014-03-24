beb0edf82294e4f9ab4d06a801b3588aaeafe890
Nero Patrick Roque <neropatrickroque@gmail.com>
2014-03-25 01:36:02 +0800

    awooo

--- application/controllers/report.php
+++ application/controllers/report.php
@@ -1775,44 +1775,22 @@ $mpdf->WriteHTML('<html><div style="text-align:center;">
             
             $data['industry'] = $this->model_pub->get_industryName($industry);
 
+            
+            /* Monthly Grads vs Employed */
             $annualgrads = $this->model_reports->get_monthlyGraduates($industry,$year);
             $hiredgrads = $this->model_reports->get_mhiredGraduates($industry,$year);
-            $monthctr = 1;
-           
             
             $reportdata = array();
             $reportdata2 = array();
+            
             foreach($annualgrads as $a){
                 $reportdata[$a['montha']] = $a['count'];
             }
+            
             foreach($hiredgrads as $b){
                 $reportdata2[$b['montha']] = $b['count'];
             }
-        
-            
-            $indexedReportData = array();
-            
-            
-            while($monthctr <= 12){
-                if(isset($reportdata[$monthctr])){
-                    $indexedReportData[$monthctr] = $reportdata[$monthctr];
-                }else{
-                    $indexedReportData[$monthctr] = 0;
-                }
-                $monthctr++;
-            }
-            
-            $indexedReportData2 = array();
-            $monthctr = 1;
-            
-            while($monthctr <=12){
-                if(isset($reportdata2[$monthctr])){
-                    $indexedReportData2[$monthctr] = $reportdata2[$monthctr];
-                }else{
-                    $indexedReportData2[$monthctr] = 0;
-                }
-                $monthctr++;
-            }
+
             $data['mreportData'] = $reportdata;
             $data['mreportData2'] = $reportdata2;
 
